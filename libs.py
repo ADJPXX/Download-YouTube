2da5f1cec8365a5f8c1affe12ce7c695f7bd4437
ADJPXX <adrieljp2005@hotmail.com>
2025-10-04 19:04:21 -0300

    Agora ele atualiza o pacote tambem

--- libs.py
+++ libs.py
@@ -10,6 +10,7 @@ def verificar_pacotes(nome_pacote):
 		else:
 			try:
 				os.system(f"pip install {nome_pacote}")
+				os.system(f"pip install --upgrade {nome_pacote}")
 
 			except Exception as e:
 				print("Ocorreu um erro ao instalar o pacote: ", e)
