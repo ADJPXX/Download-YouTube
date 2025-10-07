6d9cd40e65e451c9c04ee797f383c816487e05ce
ADJPXX <adrieljp2005@hotmail.com>
2025-10-07 03:18:45 -0300

    Agora ele atualiza o pacote de forma correta.

--- libs.py
+++ libs.py
@@ -1,18 +1,25 @@
 def verificar_pacotes(nome_pacote):
+	import os
 	try:
 		import os
 		import sys
 		import importlib
+
 		PACOTE = importlib.util.find_spec(nome_pacote)
+
 		if PACOTE is not None:
-			print("Pacote já está instalado.")
-			os.system(f"pip install --upgrade {nome_pacote}")
+			os.system("cls")
+			print(f'[✓] O pacote "{nome_pacote}" já está instalado.\nVou verificar as atualizações e se necessario atualizar.')
+			os.system(f'pip install --upgrade {nome_pacote}')
 
 		else:
 			try:
+				os.system("cls")
+				print(f"[+]Instalando o pacote {nome_pacote}...")
 				os.system(f"pip install {nome_pacote}")
 
 			except Exception as e:
+				os.system("cls")
 				print("Ocorreu um erro ao instalar o pacote: ", e)
 
 	except Exception as e:
